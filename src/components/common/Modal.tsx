76aeb0117b2eccb6bb74f0fd810088eeb7b11f09
SamerZee <samer.zughul@hotmail.com>
2022-07-13 13:23:31 +0300

    modal fix

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -8,10 +8,11 @@ type Props = JSX.IntrinsicElements['div'] & {
     onSubmit?: () => void
     submitLabel?:string
     submitDisabled?:boolean
+    extraFooterComponents?:any
 }
 
 
-const Component :React.FC<Props> = ({onClose,onSubmit,submitLabel, submitDisabled,className,children }) => {
+const Component :React.FC<Props> = ({onClose,onSubmit,submitLabel, submitDisabled, extraFooterComponents,className,children }) => {
 
     return (
         <div className={classes("relative z-10", className || "")}   aria-labelledby="modal-title" role="dialog" aria-modal="true">
@@ -21,19 +22,21 @@ const Component :React.FC<Props> = ({onClose,onSubmit,submitLabel, submitDisable
                 <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
 
                     <div
-                        className="relative bg-white rounded-lg text-left overflow-visible shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
+                        className="relative  bg-white rounded-lg text-left overflow-visible shadow-xl transform transition-all sm:my-8 sm:max-w-screen-lg sm:w-full">
                         <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                            <div className="sm:flex sm:items-start overflow-visible">
+                            <div className="sm:items-start overflow-visible">
                                 {children}
                             </div>
                         </div>
                         <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+
                             {onSubmit && <Button disabled={submitDisabled} onClick={onSubmit} type="button"
                                      className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm">{submitLabel ?? "Save"}
                             </Button>}
                             <Button onClick={onClose} type="button"
                                     className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel
                             </Button>
+                            {extraFooterComponents}
                         </div>
                     </div>
                 </div>
--- src/components/exchanges/ExchangeStatus.tsx
+++ src/components/exchanges/ExchangeStatus.tsx
@@ -15,7 +15,8 @@ const ExchangeStatus:React.FC<Props> = ({ status,onClick }) => {
                     className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-3 py-1.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Success</button>
             : <button onClick={onClick} type="button" className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-4 py-1.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Failed</button>
             : <button  type="button" className="text-white bg-yellow-500 hover:bg-yellow-800 focus:outline-none focus:ring-4 focus:ring-yellow-300 font-medium rounded-full text-sm px-4 py-1.5 text-center mr-2 mb-2 dark:bg-yellow-400 dark:hover:bg-yellow-700 dark:focus:ring-yellow-900">Running</button>
-        }</>
+        }
+       </>
     )
 }
 
