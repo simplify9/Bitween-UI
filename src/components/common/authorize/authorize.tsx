e69b95f49c9035e3006fdb38761175d8ce32c5bb
waheeb :) <waheeb@simplify9.com>
2023-02-22 15:59:32 +0300

    fix

--- src/components/common/authorize/authorize.tsx
+++ src/components/common/authorize/authorize.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {useTypedSelector} from "src/state/ReduxSotre";
 
 type Props = {
     roles: Array<"Admin" | "Editor" | "Viewer">
@@ -6,7 +7,7 @@ type Props = {
 }
 const Authorize: React.FC<Props> = ({roles, children}) => {
 
-    const role = 'Viewer'//useTypedSelector(i => i.user.userInfo?.role)
+    const role = useTypedSelector(i => i.user.userInfo?.role)
 
     // @ts-ignore
     return roles.includes(role) ? children : <section disabled>{children}</section>
