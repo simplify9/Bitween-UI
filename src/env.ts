a3cfc56e4b7caa24f08fb97910ba70b95892def1
waheeb :) <waheeb@simplify9.com>
2023-03-08 12:06:21 +0300

    Improvements + bug fixes

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,2 @@
-export const API_BASE_URL = "/api/";
-
-//export const API_BASE_URL = "https://localhost:5001/api/"
-//export const API_BASE_URL = "https://infolink.sf9.io/api/"
-
+//export const API_BASE_URL = "/api/";
+export const API_BASE_URL = "https://localhost:5001/api/"
