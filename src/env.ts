7398911e75d5c0aa6b445bbba9efe1da9e482bd3
waheeb :) <waheeb@simplify9.com>
2023-04-09 13:53:10 +0300

    env api url

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,12 @@
-export const API_BASE_URL = "/api/";
+declare global {
+    interface ImportMeta {
+        env: {
+            VITE_ApiBaseUrl: string;
+            // add other environment variables here
+        }
+    }
+}
+export const API_BASE_URL = import.meta.env.VITE_ApiBaseUrl ?? "/api/";
 //export const API_BASE_URL = "https://localhost:5001/api/"
 //export const API_BASE_URL = "https://infolink.sf9.io/API/"
 
