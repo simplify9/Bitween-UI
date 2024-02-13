eb6f4a8e517c7db01a5847196e8fa7ce383186c1
USER <ali20200215@std.psut.edu.jo>
2024-02-13 14:47:37 +0300

    isTreeFullHandler

--- src/env.ts
+++ src/env.ts
@@ -7,7 +7,7 @@ declare global {
 }
 
 //export const default_base_url = "https://localhost:5001/api/"
-export const default_base_url = "https://bitween-stg.traxis.app/api/"
+export const default_base_url = "/api/"
 
 export const API_BASE_URL = import.meta.env.VITE_ApiBaseUrl ?? default_base_url;
 //export const API_BASE_URL = "https://localhost:5001/api/"
