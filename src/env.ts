0b2dfcad8eee46787cc150fabbb7718fe4c5ec39
omar <omar@simplify9.com>
2024-04-18 13:46:59 +0300

    White label login page

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,13 @@
 const ENV = {
-    API_BASE_URL: "/api/",
+    API_BASE_URL: "https://bitween-stg.traxis.app/api/",
+    THEME: {
+        LOGIN_LOGO: "/Graphics/s9.png",
+        BITWEEN_LOGO: "/Graphics/BitweenFull.svg",
+        BITWEEEN_TEXT: "is all-in-one solution to solving integration with third parties, automating workflows\n" +
+            "                                with exchanges coming from all forms of requests, ranging from internal messages to\n" +
+            "                                files dumped on a server.",
+
+    }
 }
 export default ENV
 
