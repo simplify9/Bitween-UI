f0b9cf4ae4608f04f2e73a994b5702ca8f53ecda
USER <ali20200215@std.psut.edu.jo>
2024-05-05 15:17:08 +0300

    changes

--- src/components/exchanges/CreateExchange.tsx
+++ src/components/exchanges/CreateExchange.tsx
@@ -1,73 +1,106 @@
 import Modal from "src/components/common/Modal";
-import React, {useState} from "react";
-import {CreateXchangeModel} from "src/types/xchange";
+import React, { useState } from "react";
+import { CreateXchangeModel } from "src/types/xchange";
 import FormField from "src/components/common/forms/FormField";
-import {ChoiceEditor} from "src/components/common/forms/ChoiceEditor";
+import { ChoiceEditor } from "src/components/common/forms/ChoiceEditor";
 import SubscriptionSelector from "src/components/Subscriptions/SubscriptionSelector";
 import DocumentSelector from "src/components/Documents/DocumentSelector";
-import {apiClient} from "src/client";
+import { apiClient } from "src/client";
+import Button from "../common/forms/Button";
 
 type Props = {
-    onClose: () => void
-}
-const CreateExchange: React.FC<Props> = ({onClose}) => {
-    const [createXchange, setCreateXchange] = useState<CreateXchangeModel>({
-        documentId: null,
-        option: null,
-        data: "",
-        subscriberId: null
-    });
+  onClose: () => void;
+};
+const CreateExchange: React.FC<Props> = ({ onClose }) => {
+  const [createXchange, setCreateXchange] = useState<CreateXchangeModel>({
+    documentId: null,
+    option: null,
+    data: "",
+    subscriberId: null,
+  });
 
-    const onChange = (val: any, key: keyof CreateXchangeModel) => {
-        setCreateXchange((x) => ({...x, [key]: val}))
+  const onChange = (val: any, key: keyof CreateXchangeModel) => {
+    setCreateXchange((x) => ({ ...x, [key]: val }));
+  };
+
+  const onSubmit = async () => {
+    if (createXchange.data) {
+      const res = await apiClient.createExchange(createXchange);
+      if (res.status == 204) {
+        onClose();
+      }
     }
+  };
 
-    const onSubmit = async () => {
-        if (createXchange.data) {
-            const res = await apiClient.createExchange(createXchange)
-            if (res.status == 204) {
-                onClose()
-            }
-        }
+  const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const text = e.target.result as string;
+        onChange(text, "data");
+      };
+      reader.readAsText(file);
     }
-    return <Modal onClose={onClose} onSubmit={onSubmit}>
-        <div>
-            <div className={"flex flex-row gap-5"}>
-                <FormField title="Delivery Status">
-                    <ChoiceEditor
-                        placeholder="Select Status"
-                        value={createXchange.option}
-                        onChange={status => onChange((status), 'option')}
-                        optionTitle={(item: any) => item.title}
-                        optionValue={(item: any) => item.id}
-                        options={[
-                            {id: "0", title: "DocumentId"},
-                            {id: "1", title: "SubscriberId"},
-                        ]}/>
-                </FormField>
-                {
-                    createXchange.option === "1" && <FormField title="Subscriber Id"><SubscriptionSelector
-                        value={createXchange.subscriberId}
-                        onChange={subscription => onChange((subscription), 'subscriberId')}/></FormField>
+  };
+
+  return (
+    <Modal onClose={onClose} onSubmit={onSubmit}>
+      <div>
+        <div className={"flex flex-row gap-5"}>
+          <FormField title="Delivery Status">
+            <ChoiceEditor
+              placeholder="Select Status"
+              value={createXchange.option}
+              onChange={(status) => onChange(status, "option")}
+              optionTitle={(item: any) => item.title}
+              optionValue={(item: any) => item.id}
+              options={[
+                { id: "0", title: "DocumentId" },
+                { id: "1", title: "SubscriberId" },
+              ]}
+            />
+          </FormField>
+          {createXchange.option === "1" && (
+            <FormField title="Subscriber Id">
+              <SubscriptionSelector
+                value={createXchange.subscriberId}
+                onChange={(subscription) =>
+                  onChange(subscription, "subscriberId")
                 }
+              />
+            </FormField>
+          )}
 
-                {
-                    createXchange.option === "0" && <FormField title="Document Id">
-                        <DocumentSelector
-                            value={createXchange.documentId}
-                            onChange={docId => onChange((docId), 'documentId')}/>
-                    </FormField>
-                }
-            </div>
-
-
-            {
-                (createXchange.option) && <div className={"mt-5"}><textarea value={createXchange.data}
-                                                                            onChange={(e) => onChange((e.target.value), 'data')}
-                                                                            className={"w-full border rounded shadow"}/>
-                </div>
-            }
+          {createXchange.option === "0" && (
+            <FormField title="Document Id">
+              <DocumentSelector
+                value={createXchange.documentId}
+                onChange={(docId) => onChange(docId, "documentId")}
+              />
+            </FormField>
+          )}
         </div>
+
+        {createXchange.option && (
+          <div className={"mt-5"}>
+            <textarea
+              value={createXchange.data}
+              onChange={(e) => onChange(e.target.value, "data")}
+              className={"w-full border rounded shadow"}
+            />
+            <div className="space-y-1 font-[sans-serif] max-w-md mx-auto">
+              <span className="text-gray-500">or</span>
+              <input
+                type="file"
+                onChange={onFileChange}
+                className="w-full text-gray-500 font-medium text-sm bg-gray-100 file:cursor-pointer cursor-pointer file:border-0 file:py-2 file:px-4 file:mr-4 file:bg-primary-600 file:hover:bg-primary-500 file:transition  file:ease-in-out file:delay-100 file:text-white rounded"
+              />
+            </div>
+          </div>
+        )}
+      </div>
     </Modal>
-}
-export default CreateExchange
+  );
+};
+export default CreateExchange;
--- src/components/exchanges/ExchangeFinderPanel.tsx
+++ src/components/exchanges/ExchangeFinderPanel.tsx
@@ -1,4 +1,4 @@
-import {ExchangeFindQuery} from "src/types/xchange";
+import { ExchangeFindQuery } from "src/types/xchange";
 import React from "react";
 import FormField from "src/components/common/forms/FormField";
 import SubscriptionSelector from "src/components/Subscriptions/SubscriptionSelector";
@@ -6,152 +6,174 @@ import DateEditor from "src/components/common/forms/DateEditor";
 import TextEditor from "src/components/common/forms/TextEditor";
 import Button from "src/components/common/forms/Button";
 import ChoiceEditor from "src/components/common/forms/ChoiceEditor";
-
+import DocumentSelector from "../Documents/DocumentSelector";
 
 interface Props {
-    value: ExchangeFindQuery
-    onChange: (value: ExchangeFindQuery) => void
-    onFindRequested: () => void
-    onClear: () => void
-    onBulkRetry: () => void
-    onCreateXchange: () => void
-    isItemsSelected: boolean
+  value: ExchangeFindQuery;
+  onChange: (value: ExchangeFindQuery) => void;
+  onFindRequested: () => void;
+  onClear: () => void;
+  onBulkRetry: () => void;
+  onCreateXchange: () => void;
+  isItemsSelected: boolean;
 }
 
 type DeliveryStatus = {
-    id: string
-    title: string
-}
+  id: string;
+  title: string;
+};
 
 export const ExchangeFinderPanel: React.FC<Props> = ({
-                                                         value,
-                                                         onChange,
-                                                         onFindRequested,
-                                                         onClear,
-                                                         onCreateXchange,
-                                                         onBulkRetry,
-                                                         isItemsSelected
-                                                     }) => {
+  value,
+  onChange,
+  onFindRequested,
+  onClear,
+  onCreateXchange,
+  onBulkRetry,
+  isItemsSelected,
+}) => {
+  const handleFind = (e) => {
+    e.preventDefault();
+    onFindRequested();
+  };
 
-    const handleFind = (e) => {
-        e.preventDefault();
-        onFindRequested();
-    }
-    return (
-        <div className={"shadow px-2 mb-2  rounded-lg bg-white"} style={{zIndex: 1000}}>
-            <div className="flex flex-row justify-between items-start w-100 pb-4 pt-3 z-50 gap-x-5">
-                <div className={"grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3"}>
-                    <FormField title="Target Subscription">
-                        <SubscriptionSelector
-                            value={value.subscription}
-                            onChange={subscription => {
-                                onChange({...value, subscription})
-                            }}/>
-                    </FormField>
-                    <FormField title="Creation Time From">
-                        <DateEditor onChange={(t) => onChange({
-                            ...value,
-                            creationDateFrom: t
-
-                        })} value={value.creationDateFrom}/>
-                    </FormField>
-                    <FormField title="Creation Time To">
-                        <DateEditor onChange={(t) => onChange({
-                            ...value,
-                            creationDateTo: t
-                        })} value={value.creationDateTo}/>
-                    </FormField>
-                    <FormField title="Delivery Status">
-                        <ChoiceEditor
-                            placeholder="Select Status"
-                            value={value.status}
-                            onChange={status => onChange({...value, status})}
-                            optionTitle={(item: DeliveryStatus) => item.title}
-                            optionValue={(item: DeliveryStatus) => item.id}
-                            options={[
-                                {id: "0", title: "Running"},
-                                {id: "1", title: "Success"},
-                                {id: "2", title: "Bad response"},
-                                {id: "3", title: "Failed"}
-                            ]}/>
-                    </FormField>
-                    <FormField title="Promoted Properties">
-                        <TextEditor placeholder="Promoted Properties" value={value.promotedProperties}
-                                    onChange={(t) => onChange({...value, promotedProperties: t})}/>
-                    </FormField>
-                    <FormField title="ID">
-                        <TextEditor placeholder="ID" value={value.id}
-                                    onChange={(t) => onChange({...value, id: t})}/>
-                    </FormField>
-                    <FormField title="Correlation ID">
-                        <TextEditor placeholder="Correlation ID" value={value.correlationId}
-                                    onChange={(t) => onChange({...value, correlationId: t})}/>
-                    </FormField>
-
-                </div>
-                <div className={"w-[300px] items-end justify-end flex flex-col"}>
-                    <div className={" flex w-full items-end justify-end mx-1 "}>
-                        <FormField title="Refresh every" className={"w-full"}>
-                            <ChoiceEditor
-                                placeholder="Select Status"
-                                value={value.fetchInterval?.toString()}
-                                onChange={fetchInterval => onChange({
-                                    ...value,
-                                    fetchInterval: Number(fetchInterval)
-                                })}
-                                optionTitle={(item: DeliveryStatus) => item.title}
-                                optionValue={(item: DeliveryStatus) => item.id}
-                                options={[
-                                    {id: "2000", title: "2 Seconds"},
-                                    {id: "3000", title: "3 Seconds"},
-                                    {id: "5000", title: "5 Seconds"},
-                                    {id: "8000", title: "8 Seconds"},
-                                    {id: "13000", title: "13 Seconds"},
-                                ]}/>
-                        </FormField>
-
-                    </div>
-                    <div className={"pt-1  w-full"}>
-                        <div className={"flex flex-row  "}>
-                            <Button
-                                onClick={() => {
-                                    onCreateXchange()
-                                }}
-                            >
-                                Create Xchange
-                            </Button>
-                            {
-                                isItemsSelected && <Button
-                                    onClick={onBulkRetry}
-                                >
-                                    Bulk retry
-                                </Button>
-                            }
-
-                        </div>
-                        <div className={"flex flex-row pt-1"}>
-                            <Button
-                                variant={"secondary"}
-                                onClick={onClear}
-                            >
-                                Clear
-                            </Button>
-                            <Button
-                                onClick={handleFind}
-                            >
-                                Search
-                            </Button>
-                        </div>
-                    </div>
-
-
-                </div>
-
-            </div>
-
+  return (
+    <div
+      className={"shadow px-2 mb-2  rounded-lg bg-white"}
+      style={{ zIndex: 1000 }}
+    >
+      <div className="flex flex-row justify-between items-start w-100 pb-4 pt-3 z-50 gap-x-5">
+        <div
+          className={
+            "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3"
+          }
+        >
+          <FormField title="Target Subscription">
+            <SubscriptionSelector
+              value={value.subscription}
+              onChange={(subscription) => {
+                onChange({ ...value, subscription });
+              }}
+            />
+          </FormField>
+          <FormField title="Document">
+            
+            <DocumentSelector
+              value={value.documentId}
+              onChange={(t) =>
+                onChange({
+                    ...value,
+                    documentId:t,
+                  })
+              }
+            />
+          </FormField>
+          <FormField title="Creation Time From">
+            <DateEditor
+              onChange={(t) =>
+                onChange({
+                  ...value,
+                  creationDateFrom: t,
+                })
+              }
+              value={value.creationDateFrom}
+            />
+          </FormField>
+         
+          <FormField title="Creation Time To">
+            <DateEditor
+              onChange={(t) =>
+                onChange({
+                  ...value,
+                  creationDateTo: t,
+                })
+              }
+              value={value.creationDateTo}
+            />
+          </FormField>
+          <FormField title="Delivery Status">
+            <ChoiceEditor
+              placeholder="Select Status"
+              value={value.status}
+              onChange={(status) => onChange({ ...value, status })}
+              optionTitle={(item: DeliveryStatus) => item.title}
+              optionValue={(item: DeliveryStatus) => item.id}
+              options={[
+                { id: "0", title: "Running" },
+                { id: "1", title: "Success" },
+                { id: "2", title: "Bad response" },
+                { id: "3", title: "Failed" },
+              ]}
+            />
+          </FormField>
+          <FormField title="Promoted Properties">
+            <TextEditor
+              placeholder="Promoted Properties"
+              value={value.promotedProperties}
+              onChange={(t) => onChange({ ...value, promotedProperties: t })}
+            />
+          </FormField>
+          <FormField title="ID">
+            <TextEditor
+              placeholder="ID"
+              value={value.id}
+              onChange={(t) => onChange({ ...value, id: t })}
+            />
+          </FormField>
+          <FormField title="Correlation ID">
+            <TextEditor
+              placeholder="Correlation ID"
+              value={value.correlationId}
+              onChange={(t) => onChange({ ...value, correlationId: t })}
+            />
+          </FormField>
         </div>
-    )
-}
-
-
-
+        <div className={"w-[300px] items-end justify-end flex flex-col"}>
+          <div className={" flex w-full items-end justify-end mx-1 "}>
+            <FormField title="Refresh every" className={"w-full"}>
+              <ChoiceEditor
+                placeholder="Select Status"
+                value={value.fetchInterval?.toString()}
+                onChange={(fetchInterval) =>
+                  onChange({
+                    ...value,
+                    fetchInterval: Number(fetchInterval),
+                  })
+                }
+                optionTitle={(item: DeliveryStatus) => item.title}
+                optionValue={(item: DeliveryStatus) => item.id}
+                options={[
+                  { id: "2000", title: "2 Seconds" },
+                  { id: "3000", title: "3 Seconds" },
+                  { id: "5000", title: "5 Seconds" },
+                  { id: "8000", title: "8 Seconds" },
+                  { id: "13000", title: "13 Seconds" },
+                ]}
+              />
+            </FormField>
+          </div>
+          <div className={"pt-1  w-full"}>
+            <div className={"flex flex-row  "}>
+              <Button
+                onClick={() => {
+                  onCreateXchange();
+                }}
+              >
+                Create Xchange
+              </Button>
+              {isItemsSelected && (
+                <Button onClick={onBulkRetry}>Bulk retry</Button>
+              )}
+            </div>
+            <div className={"flex flex-row pt-1"}>
+              <Button variant={"secondary"} onClick={onClear}>
+                Clear
+              </Button>
+              <Button onClick={handleFind}>Search</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
