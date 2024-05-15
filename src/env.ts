d0922e2ae2e67a21d3775342aa8df6f269254829
waheeb :) <waheeb@simplify9.com>
2024-05-15 17:33:39 +0300

    fix

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,14 @@
 const ENV = {
     API_BASE_URL: "/api/",
+    CONFIG: {
+        XCHANGE_REFRESH_DEFAULT_INTERVAL: 5000,
+        XCHANGE_REFRESH_DEFAULT_INTERVAL_OPTIONS: [
+            {id: (5 * 1000), title: "5 Seconds"},
+            {id: (30 * 1000), title: "30 Seconds"},
+            {id: (60 * 1000), title: "1 Minute"},
+            {id: (5 * 60 * 1000), title: "5 Minutes"}
+        ]
+    },
     THEME: {
         LOGIN_LOGO: "/Graphics/s9.png",
         BITWEEN_LOGO: "/Graphics/BitweenFull.svg",
